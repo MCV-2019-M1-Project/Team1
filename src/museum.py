61d47808faad67b1034185959a3bba0f66199fda
Ruben Bagan Benavides <ruben.bagan@gmail.com>
2019-10-03 00:28:18 +0200

    basic usage of the current classes

--- src/museum.py
+++ src/museum.py
@@ -1,4 +1,11 @@
 from image import Image
+from histogram import Histogram
+import cv2
 
+# Testing the image class
 img = Image('bbdd/bbdd_00000.jpg')
 print(img.img)
+histogram = Histogram.calc_histogram(img, 0)
+print(histogram)
+equalize = Histogram.calc_equalize_hist(img)
+print(equalize)
