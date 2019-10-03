3ac691a1ce2ee8379134931463f7a34fbe1125fa
Ruben Bagan Benavides <ruben.bagan@gmail.com>
2019-10-03 22:27:56 +0200

    add example of usage to remove a background

--- src/museum.py
+++ src/museum.py
@@ -11,12 +11,12 @@ print('Img color space', img.color_space)
 print('Img the img itself', img.img)
 print('Get the basic hist', img.calc_histogram(0))
 print('Get the equalized hist', img.calc_equalize_hist())
-print('change to HSV color space')
-img.color_space = 'HSV'
-print('Img color space', img.color_space)
 
 museum_item = MuseumItem(img)
 export_museum_item(museum_item)
 
 museum_item = import_museum_item(img.filename)
 print('Img filename restored', museum_item.image.filename)
+
+img_background = img.get_background()
+cv2.imwrite('example.png', img_background)
