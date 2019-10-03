d8f693b9f6add618e6eda5ac5e19fd0ba8a3cdf2
Ruben Bagan Benavides <ruben.bagan_benavides@roche.com>
2019-10-03 11:59:34 +0200

    basic usage of image class

--- src/museum.py
+++ src/museum.py
@@ -3,3 +3,11 @@ import cv2
 
 # Testing the image class
 img = Image('bbdd/bbdd_00000.jpg')
+print('Img filename', img.filename)
+print('Img color space', img.color_space)
+print('Img the img itself', img.img)
+print('Get the basic hist', img.calc_histogram(0))
+print('Get the equalized hist', img.calc_equalize_hist())
+print('change to HSV color space')
+img.color_space = 'HSV'
+print('Img color space', img.color_space)
