5f1b2fc1d8962062794763480b20dd347873f999
Ruben Bagan Benavides <ruben.bagan@gmail.com>
2019-10-04 18:38:40 +0200

    apply pep9

--- src/Background_remover.py
+++ src/Background_remover.py
@@ -4,8 +4,8 @@ from scipy.signal import find_peaks
 import os
 import glob
 
-class BackgroundRemoverBase:
 
+class BackgroundRemoverBase:
     def __init__(self):
         self.mask = None
 
@@ -14,10 +14,7 @@ class BackgroundRemoverBase:
 
 
 class BasicRemovingStrategy(BackgroundRemoverBase):
-
-    def __init__(self,
-                 height_ratio=10,
-                 distance_between_peaks=10):
+    def __init__(self, height_ratio=10, distance_between_peaks=10):
         super().__init__()
         self.height_ratio = height_ratio
         self.distance_between_peaks = distance_between_peaks
@@ -26,9 +23,10 @@ class BasicRemovingStrategy(BackgroundRemoverBase):
         """
             TODO
         """
-
         def my_find_peaks(array):
-            peaks, _ = find_peaks(array, height=np.max(array) / self.height_ratio, distance=self.distance_between_peaks)
+            peaks, _ = find_peaks(array,
+                                  height=np.max(array) / self.height_ratio,
+                                  distance=self.distance_between_peaks)
             return peaks
 
         gray = cv2.cvtColor(numpy_image, cv2.COLOR_BGR2GRAY)
@@ -38,7 +36,8 @@ class BasicRemovingStrategy(BackgroundRemoverBase):
         left_cd = my_find_peaks(col_var[:len(col_var) // 2])
         left = left_cd[0]
 
-        right_cd = my_find_peaks(col_var[len(col_var) // 2:]) + len(col_var) // 2
+        right_cd = my_find_peaks(
+            col_var[len(col_var) // 2:]) + len(col_var) // 2
         right = right_cd[-1]
 
         row_var = np.abs(np.gradient(gray.mean(1)))
@@ -46,10 +45,13 @@ class BasicRemovingStrategy(BackgroundRemoverBase):
         top_cd = my_find_peaks(row_var[:len(row_var) // 2])
         top = top_cd[0]
 
-        bottom_cd = my_find_peaks(row_var[len(row_var) // 2:]) + len(row_var) // 2
+        bottom_cd = my_find_peaks(
+            row_var[len(row_var) // 2:]) + len(row_var) // 2
         bottom = bottom_cd[-1]
 
-        self.mask = np.zeros(shape=(numpy_image.shape[0], numpy_image.shape[1]), dtype=np.uint8)
+        self.mask = np.zeros(shape=(numpy_image.shape[0],
+                                    numpy_image.shape[1]),
+                             dtype=np.uint8)
         self.mask[top:bottom, left:right] = 255
         cropped_image = numpy_image[top:bottom, left:right].copy()
         return cropped_image
