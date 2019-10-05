2544129f35f328c9b2e425be9b46d6fee9f36728
mrfarge <m.rodriguezfarge@gmail.com>
2019-10-05 11:28:33 +0200

    Update k_retrieval.py

--- src/k_retrieval.py
+++ src/k_retrieval.py
@@ -1,9 +1,7 @@
 def k_retrieval(distances_list, k):
     
     """
-    
     Returns the K more similar images of the database from a list of distance obj
-    
     """
     
     maximization_required = distances_list[0].maximization_required
@@ -11,6 +9,5 @@ def k_retrieval(distances_list, k):
     
     distances_list.sort(key=lambda x: x.distance, reverse=maximization_required)
     k_distances=distances_list[0 : k+1]
-
-        
+   
     return k_distances
