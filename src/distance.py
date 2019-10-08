456a960bfb87125a5e6e87f8b967603d9f0c2ea4
Ruben Bagan Benavides <ruben.bagan@gmail.com>
2019-10-08 20:35:15 +0200

    make the distances to be functions

--- src/distance.py
+++ src/distance.py
@@ -2,75 +2,27 @@ import numpy as np
 from scipy.spatial import distance
 from cv2 import cv2
 
-class Distance:
-    def __init__(self, query_museum_item, db_museum_item):
-        self._query_museum_item = query_museum_item
-        self._db_museum_item = db_museum_item
-        self._distance = None
-        self._method = None
-        self._maximization = False
 
-    @property
-    def query_museum_item(self):
-        return self._query_museum_item
+def euclidean(histogram1, histogram2):
+    return distance.euclidean(histogram1, histogram2)
 
-    @property
-    def db_im(self):
-        return self._db_museum_item
 
-    @property
-    def method(self):
-        return self._method
+def l1_dist(histogram1, histogram2):
+    return distance.cityblock(histogram1, histogram2)
 
-    @property
-    def distance(self):
-        return self._distance
 
-    @property
-    def maximization(self):
-        """
-        True if method requires maximization, False if minimization
-        """
-        return self._maximization
+def x2_dist(histogram1, histogram2):
+    return np.sum(
+        (histogram1 - histogram2)**2 / (histogram1 + histogram2 + 1e-6))
 
-    def calc_dist(self, similarity_method):
-        """
-        Calc the desired distance
-        
-        """
-        query_hist = self._query_museum_item.histogram
-        db_hist = self._db_museum_item.histogram
 
-        if similarity_method == "euclidean":
-            self._method = "euclidean"
-            self._distance = distance.euclidean(query_hist, db_hist)
-            self._maximization = False
+def intersection(histogram1, histogram2):
+    return cv2.compareHist(histogram1, histogram2, cv2.HISTCMP_INTERSECT)
 
-        elif similarity_method == "L1_dist":
-            self._method = "L1_dist"
-            self._distance = distance.cityblock(query_hist, db_hist)
-            self._maximization = False
 
-        elif similarity_method == "x2_dist":
-            self._method = "x2_dist"
-            self._distance = np.sum(
-                (query_hist - db_hist)**2 / (query_hist + db_hist + 1e-6))
-            self._maximization = False
+def hellinger(histogram1, histogram2):
+    return cv2.compareHist(histogram1, histogram2, cv2.HISTCMP_HELLINGER)
 
-        elif similarity_method == "intersection":
-            self._method = "intersection"
-            self._distance = cv2.compareHist(query_hist, db_hist,
-                                             cv2.HISTCMP_INTERSECT)
-            self._maximization = True
 
-        elif similarity_method == "hellinger":
-            self._method = "hellinger"
-            self._distance = cv2.compareHist(query_hist, db_hist,
-                                             cv2.HISTCMP_HELLINGER)
-            self._maximization = False
-
-        elif similarity_method == "correlation":
-            self._method = "correlation"
-            self._distance = cv2.compareHist(query_hist, db_hist,
-                                             cv2.HISTCMP_CORREL)
-            self._maximization = True
+def correlation(histogram1, histogram2):
+    return cv2.compareHist(histogram1, histogram2, cv2.HISTCMP_CORREL)
