287af927f5206ae164fa97ce38339e42dbbd91f6
mariavila <mariava.1213@gmail.com>
2019-10-12 16:57:27 +0200

    Added multiresolution histograms

--- src/image_descriptors.py
+++ src/image_descriptors.py
@@ -33,10 +33,10 @@ def image_filtering(self, ddepth, kernel):
 
     return cv2.filter2D(self._img, ddepth, kernel)
 
-def calc_histogram(painting, channel, hist_size=[256], ranges=[0, 256], mask=None):
+def calc_normalized_histogram(image, channel, hist_size=[256], ranges=[0, 256], masks=None):
     """
     Args:
-        - painting: a painting instance object
+        - image: image where the histogram is going to be computed
         - channel:
             * 0 if is a grayscale image
             * 0 calculate blue histogram
@@ -66,37 +66,29 @@ def calc_histogram_1D_luminance_and_2D_chrominance(painting, masks=None):
     Returns
         The normalized 1D histogram of the iluminance concatenated with the normalized 2D histogram of the chrominance for every mask
     """
-    ##Convert to hsv color space
+    #Convert to hsv color space
     painting.color_space = 'YCrCb'
     if masks is None:
         #Compute histogram 2D
-        hist2D =calc_histogram(painting, [1,2], [256, 256], [0, 256, 0, 256], None)
-        #Normalize histogram 2D
-        hist2D_norm = cv2.normalize(hist2D, 0.0, 1.0)
+        hist2D =calc_normalized_histogram(painting.img, [1,2], [256, 256], [0, 256, 0, 256], None)
         #Flatten 2D histogram
-        hist2D_flatten = hist2D_norm.flatten()
+        hist2D_flatten = hist2D.flatten()
         #Compute histogram 1D
-        hist1D =calc_histogram(painting, [0], [256], [0, 256], None)
-        #Normalize histogram 1D
-        hist1D_norm = cv2.normalize(hist1D, 0.0, 1.0)
+        hist1D =calc_normalized_histogram(painting.img, [0], [256], [0, 256], None)
         #Join and return features
-        return np.append(hist2D_flatten, hist1D_norm)
+        return np.append(hist2D_flatten, hist1D)
     else:
         hists = np.empty(shape=[0])
         for i in masks:
             #Compute histogram 2D
-            hist2D =calc_histogram(painting, [1,2], [256, 256], [0, 256, 0, 256], [i])
-            #Normalize histogram 2D
-            hist2D_norm = cv2.normalize(hist2D, 0.0, 1.0)
+            hist2D =calc_normalized_histogram(painting.img, [1,2], [256, 256], [0, 256, 0, 256], [i])
             #Flatten 2D histogram
-            hist2D_flatten = hist2D_norm.flatten()
+            hist2D_flatten = hist2D.flatten()
             #Compute histogram 1D
-            hist1D =calc_histogram(painting, [0], [256], [0, 256], [i])
-            #Normalize histogram 1D
-            hist1D_norm = cv2.normalize(hist1D, 0.0, 1.0)
-            #Join and append features to hists variable
-            hists = np.append(hists, hist1D_norm)
-            hists = np.append(hists, hist2D_flatten)
+            hist1D =calc_normalized_histogram(painting.img, [0], [256], [0, 256], [i])
+            #Join and return features
+            hist_joined = np.append(hist2D_flatten, hist1D)
+            hists = np.append(hists, hist_joined)
         return hists
 
 def calc_block_masks(painting, n_blocks):
@@ -116,3 +108,35 @@ def calc_block_masks(painting, n_blocks):
             mask_aux[int(i*painting.img.shape[0]/n_blocks):int((i + 1)*painting.img.shape[0]/n_blocks)][int(j*painting.img.shape[0]/n_blocks):int((j + 1)*painting.img.shape[0]/n_blocks)] = 1
             masks.append(mask_aux)
     return masks
+
+def compute_multires_histograms(painting, list_gauss_filter_sizes, channel, hist_size=[256], ranges=[0, 256], masks=None):
+    """
+    Computes the multiresolution histograms where each resolution is obtained applying a gaussian filter of the size list_gauss_filter_sizes[i]*list_gauss_filter_sizes[i]
+    Args:
+        - painting: a painting instance object
+        - list_gauss_filter_sizes: list of the sizes of the gaussaian filter that are going to be used to obtain the different resolutions
+        - channel:
+            * 0 if is a grayscale image
+            * 0 calculate blue histogram
+            * 1 calculate green histogram
+            * 2 calculate red histogram
+        - hist_size: this represents our BIN count. By default is full scale.
+        - ranges: histogram range. By default is full range
+        - mask: None for full image or list of masks
+    Returns
+        Array of the concatenation of the histograms at the different resolutions
+    """
+    hists = np.empty(shape=[0])
+    if masks is None:
+        for i in list_gauss_filter_sizes:
+            im_resized = cv2.GaussianBlur(painting.img,(i,i),0)
+            hist_aux = calc_normalized_histogram(im_resized, channel, hist_size, ranges, masks)
+            hists = np.append(hists, hist_aux)
+        return hists
+    else:
+        for j in masks:
+            for i in list_gauss_filter_sizes:
+                im_resized = cv2.GaussianBlur(painting.img,(i,i),0)
+                hist_aux = calc_normalized_histogram(im_resized, channel, hist_size, ranges, [j])
+                hists = np.append(hists, hist_aux)
+        return hists
