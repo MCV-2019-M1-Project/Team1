1e2860d3176ce7a9c9e1e676d55136bb2edce371
mrfarge <m.rodriguezfarge@gmail.com>
2019-10-24 19:53:22 +0200

    Improved text detection and update text_recognition
    improved text detection merging both methods used in the last delivery
    
    Update recognition doing with implemented functions to save text result if desired and exemple

--- src/text_detector.py
+++ src/text_detector.py
@@ -1,6 +1,5 @@
-import  cv2
+import cv2
 import matplotlib.pyplot as plt
-from skimage.morphology import square
 import skimage.segmentation
 import itertools
 import numpy as np
@@ -16,20 +15,20 @@ def morphological_processing(gray_im, thr, blur_method, post_thresholding):
     S5 = skimage.morphology.disk(5)
     S2 = skimage.morphology.disk(2)
     S3 = skimage.morphology.disk(3)
-    
-    
+
+
     #Applying closing to the image
     closing = cv2.morphologyEx(gray_im, cv2.MORPH_CLOSE, S5)
-    
+
     #Applying opening to the image
     opening = cv2.morphologyEx(gray_im, cv2.MORPH_OPEN, S5)
-       
+
     #difference between closing of the image and opening
     Im_op_clo_diff = closing - opening
     if blur_method == 'gaussian':
         #Gaussian filter to smooth the image and reduce noise for a better binarization
         blur_img = cv2.GaussianBlur(Im_op_clo_diff,(5,5),3)
-        
+
     elif blur_method == 'kernel':
         #2D convolution filter to smooth the image and reduce noise for a better binarization
         blur_img = low_pass_filter(Im_op_clo_diff)
@@ -45,13 +44,13 @@ def morphological_processing(gray_im, thr, blur_method, post_thresholding):
         #dilation and erosion of the binarized image
         dilated_im = cv2.dilate(th3, S3, iterations = 3)
         processed_im = cv2.erode(dilated_im, S2, iterations = 2)
-    
+
 
     #find contours of the connected components in the binarized image
     im2, cont, hierarchy = cv2.findContours(processed_im, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-    
+
     rgb = cv2.cvtColor(gray_im, cv2.COLOR_GRAY2RGB)
-    
+
     return cont, rgb, dilated_im
 
 def remove_overlapped(over_Rects, extt_rects, ind1, ind2):
@@ -62,38 +61,40 @@ def remove_overlapped(over_Rects, extt_rects, ind1, ind2):
         x2, y2, w2, h2 = over_Rects
         x3=min(x1,x2)
         y3=min(y1,y2)
-        w3=max(x2+w2-x1, x1+w1-x2) 
+        w3=max(x2+w2-x1, x1+w1-x2)
         h3=max(h1,h2)
         extt_rects[ind1] = "False"
         extt_rects[ind2] = "False"
         new_rect = (x3, y3, w3, h3)
         extt_rects.append(new_rect)
-        
+
         return extt_rects
 
-def get_max_rectangle(rects): 
+def get_max_rectangle(rects):
     """
     Get the rectangle with biggest area
-    """  
+    """
     w_list = [item[2] for item in rects]
     h_list = [item[3] for item in rects]
     area_list = [a*b for a,b in zip(w_list,h_list)]
-    max_area = max(area_list)
-    max_rect_ind = area_list.index(max_area)
-    x, y, w, h = rects[max_rect_ind]
-    
+    if area_list:
+        max_area = max(area_list)
+        max_rect_ind = area_list.index(max_area)
+        x, y, w, h = rects[max_rect_ind]
+    else:
+        return 0,0,0,0
     return rects[max_rect_ind]
 
 def erase_overlapped_regions(rects, im_w, im_h):
-    
-    """ 
+
+    """
     Remove the overlapped rectangles and merge the ones that are very near
     """
 
     index_list=[]
     ext_rects = rects[:]
     overlapped = []
-#    a = 
+#    a =
     for i in range(0,len(ext_rects)):
         overlapped[:] = []
         b = len(ext_rects)
@@ -102,20 +103,20 @@ def erase_overlapped_regions(rects, im_w, im_h):
 
                 x1, y1, w1, h1 = ext_rects[i]
                 x2, y2, w2, h2 = ext_rects[jj]
-                
+
                 if x1>x2 and abs(x1-x2-w2)<0.040*im_w and -0.01*im_w<y1-y2<0.01*im_h:
                     overlapped.append(jj)
-                    
+
                 else:
                     overlapped.append(-1)
             else:
                 overlapped.append(-1)
                 pass
-                
+
         index_list[:]=[]
-              
+
         overlapped_list = [item != -1 for item in overlapped]
-        elem_overlapped = list(itertools.compress(ext_rects, overlapped_list))   
+        elem_overlapped = list(itertools.compress(ext_rects, overlapped_list))
         for el in overlapped:
             if el != -1:
                 index_list.append(el)
@@ -125,18 +126,18 @@ def erase_overlapped_regions(rects, im_w, im_h):
             if it != "False" and ext_rects[i] != "False":
                 ext_rects = remove_overlapped(elem_overlapped[it], ext_rects, i, index_list[it])
     rectangles_no_overlapped = [item != "False" for item in ext_rects]
-    ext_rects = list(itertools.compress(ext_rects, rectangles_no_overlapped)) 
+    ext_rects = list(itertools.compress(ext_rects, rectangles_no_overlapped))
     if False in rectangles_no_overlapped:
         ext_rects = erase_overlapped_regions(ext_rects, im_w, im_h)
     else:
         pass
 
-        
-        
+
+
     return ext_rects
-    
+
 def erase_inside_regions(rects):
-    """ 
+    """
     Remove the rectangles contained in others
     """
     num_rects = len(rects)-1
@@ -147,12 +148,12 @@ def erase_inside_regions(rects):
         for j in range(num_rects+1):
             x1, y1, w1, h1 = rects[i]
             x2, y2, w2, h2 = rects[j]
-            
+
             if (x2 > x1 and x2+w2 < x1+w1 and y2 > y1 and y2+h2 < y1+h1):
                 #x1 contains x1
 
                 contains.append(1)
-               
+
             else:
                 contains.append(-1)
 
@@ -163,7 +164,7 @@ def erase_inside_regions(rects):
 
 def text_extraction(im_gray):
     contours, im_rgb, dilation = morphological_processing(im_gray, 30, 'kernel', 'dilationErosion')
-    
+
     im_x, im_y = im_gray.shape
     im_area =im_x*im_y
     rects = []
@@ -177,7 +178,7 @@ def text_extraction(im_gray):
              if w and h aren't too small,
                 area of the rectangle isn't too small or big respect the image area
                 and ratio between h/w is bigger than 0.08 and smaller than 0.6
-            we append the rectangle 
+            we append the rectangle
              """
             rect = (x, y, w, h)
             rects.append(rect)
@@ -186,10 +187,12 @@ def text_extraction(im_gray):
     detected_rects = get_max_rectangle(no_overlapped_rects)
     #for rectangle in no_overlapped_rects:
     x, y, w, h = detected_rects
+#    cv2.rectangle(im_rgb, (x, y), (x+w, y+h), (255, 0, 0), 5)
+
     # List of rectangles to return the coordinates as required (tlx, tly, brx, bry)
     det_rect = (x, y, x+w, y+h)
-        
-    
+
+
     return det_rect
 
 
@@ -198,13 +201,13 @@ def detect_text_box(gray_im, plot_results):
     Detect the bounding box of the text
 
     args:
-    - gray_im: image in grayscale 
+    - gray_im: image in grayscale
     - plot_results: if true imshow of the orginal gray image with the bounding box contour superposed
     output:
-    - rects: Coordinates, width and height 
+    - rects: Coordinates, width and height
     """
     contours, im_rgb, dilation = morphological_processing(gray_im, 2, 'gaussian', 'dilation')
-    
+
     im_x, im_y = gray_im.shape
     im_area =im_x*im_y
     rects = []
@@ -218,12 +221,12 @@ def detect_text_box(gray_im, plot_results):
              if w and h aren't too small,
                 area of the rectangle isn't too small or big respect the image area
                 and ratio between h/w is bigger than 0.08 and smaller than 0.6
-            we append the rectangle 
+            we append the rectangle
              """
             rect = (x, y, w, h)
             rects.append(rect)
-            
-       
+
+
     detected_rects = []
     if rects:
         """
@@ -231,32 +234,92 @@ def detect_text_box(gray_im, plot_results):
         """
         outside_rects = erase_inside_regions(rects)
         no_overlapped_rects = erase_overlapped_regions(outside_rects, im_x, im_y)
-                
-        list_dilation = [] 
-    
+
+        list_dilation = []
+
         for rectangle in no_overlapped_rects:
             x, y, w, h = rectangle
             # List of rectangles to return the coordinates as required (tlx, tly, brx, bry)
             det_rect = (x, y, x+w, y+h)
             detected_rects.append(det_rect)
-            list_dilation.append(sum(sum(dilation[y:(y+h), x:x+w]))) 
-        #take the rectangle with biggest values inside    
-        max_dilation_rect=max(list_dilation)
-        text_rect = detected_rects[list_dilation.index(max_dilation_rect)]
+            list_dilation.append(sum(sum(dilation[y:(y+h), x:x+w])))
+        #take the rectangle with biggest values inside
+        max_dilation_rect = max(list_dilation)
+        text_rect_1 = detected_rects[list_dilation.index(max_dilation_rect)]
+        
+
+        text_rect_2 = text_extraction(gray_im)
+        text_rect = merge_text_detection_methods(text_rect_1, text_rect_2, im_x, im_y, dilation)
         cv2.rectangle(im_rgb, (text_rect[0], text_rect[1]), (text_rect[2], text_rect[3]), (255, 0, 0), 5)
-          
-            
     else:
         """
         Else if there are no rectangles detected in the image
         """
-        detected_rects = text_extraction(gray_im)
-        cv2.rectangle(im_rgb, (detected_rects[0], detected_rects[1]), (detected_rects[2], detected_rects[3]), (255, 0, 0), 5)
-
+        text_rect = text_extraction(gray_im)
 
     if plot_results:
         fig= plt.figure(figsize=(11,15))
-        plt.imshow(im_rgb) 
+        plt.imshow(im_rgb)
     else:
         pass
-    return detected_rects
+    return text_rect
+
+def get_text_mask(im, bounding_box):
+    "Receives an image WITH JUST ONE PAINTING and returns the mask with the pixels inside the text bbox to 0"
+    tlx, tly, brx, bry = bounding_box
+    mask = np.ones(shape=(im.shape[0], im.shape[1]),
+                    dtype=np.uint8)
+
+    mask[tly:bry, tlx:brx] = 0
+    return mask
+
+def get_text_mask_BGR(im):
+    "Receives an image WITH JUST ONE PAINTING and returns the mask with the pixels inside the text bbox to 0"
+    tlx, tly, brx, bry = detect_text_box(cv2.cvtColor(im, cv2.COLOR_BGR2GRAY), False)
+    mask = np.ones(shape=(im.shape[0], im.shape[1]),
+                    dtype=np.uint8)
+
+    mask[tly:bry, tlx:brx] = 0
+    return mask
+
+def merge_text_detection_methods(m1_cords, m2_cords, im_w, im_h, dilation):
+    
+    x1, y1, xw1, yh1 = m1_cords
+    x2, y2, xw2, yh2 = m2_cords
+    possible_bbox1 = erase_vertical_rects(x1, y1, xw1-x1, yh1-y1)
+    possible_bbox2 = erase_vertical_rects(x2, y2, xw2-x2, yh2-y2)
+    rects = []
+    rects.append((x1, y1, xw1-x1, yh1-y1))
+    rects.append((x2, y2, xw2-x2, yh2-y2))
+    
+    if possible_bbox1 == False and possible_bbox2 == True:
+        text_box = m2_cords
+    elif possible_bbox2 == False and possible_bbox1 == True:
+        text_box = m1_cords
+    elif possible_bbox2 == True and possible_bbox1 == True:
+        no_contained_rects = erase_inside_regions(rects)
+        if len(no_contained_rects)>1:
+            no_overlapped = erase_overlapped_regions(no_contained_rects, im_w, im_h)
+            if len(no_overlapped)>1: 
+                dil1 = dilation[y1:yh1, x1:xw1]
+                dil2 = dilation[y2:yh2, x2:xw2]
+                if sum(sum(dil1))>sum(sum(dil2)):
+                    text_box = m1_cords
+                elif sum(sum(dil1))<sum(sum(dil2)):
+                    text_box = m2_cords
+            elif len(no_overlapped)==1:
+                x, y, w, h = no_overlapped[0]
+                text_box = x, y, x+w, y+h
+        elif len(no_contained_rects)==1:
+            x, y, w, h = no_contained_rects[0]
+            text_box = x, y, x+w, y+h
+    elif possible_bbox1 == False and possible_bbox2 == False:
+        text_box = (0,0,0,0)
+    return text_box
+
+def erase_vertical_rects(x, y, w, h):
+    if w*0.4>=h:
+        possible_bbox = True
+    else:
+        possible_bbox = False
+    return possible_bbox
--- src/text_recognition.py
+++ src/text_recognition.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import cv2
 import pytesseract
 import matplotlib.pyplot as plt
@@ -5,7 +7,7 @@ from text_detector import detect_text_box
 import difflib
 import os.path as path
 import platform
-
+from background_remover import get_bbox
 
 if platform.system() == 'Windows':
     two_up =  path.abspath(path.join(__file__ ,"../.."))
@@ -24,9 +26,15 @@ def text_recognition(image, config = '-l eng --oem 1 --psm 3',plot_rect = False)
     ret,th3 = cv2.threshold(txt_im, 160, 255, cv2.THRESH_BINARY)
     text = pytesseract.image_to_string(th3, config=config)
     if plot_rect:
+        fig= plt.figure(figsize=(11,15))
         plt.imshow(th3, 'gray') 
+    
     return text
 
+def save_text(filename, text, mode):
+    f = open(filename + ".txt", mode)
+    f.write(text)
+    
 def image_text_retrieval(text, text_list):
     """
     Returns the index of the closest string in the text_list of the input text
@@ -36,13 +44,23 @@ def image_text_retrieval(text, text_list):
     match_index = text_list.index(closest_match[0])
     
     return match_index
-    
-### Ex   
-path = 'D:\\Users\\USUARIO\\Documents\\M1.IntroductionToHumanAndVC\\M1.P2\\qsd1_w2\\00003.jpg' 
 
-image = cv2.imread(path)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-config = ('-l eng --oem 1 --psm 3')
-obtained_text = text_recognition(gray, config, False)
-print(obtained_text)
+#### Ex   
+#config = ('-l eng --oem 1 --psm 3')
+#
+#for i in range(29, -1, -1):
+#    n = str(i).zfill(2)
+#    path = 'D:\\Users\\USUARIO\\Documents\\M1.IntroductionToHumanAndVC\\M1.P3\\qsd2_w3\\000%s.jpg' % (n)
+##path = 'D:\\Users\\USUARIO\\Documents\\M1.IntroductionToHumanAndVC\\M1.P3\\qsd2_w3\\00024.jpg'
+#    image = cv2.imread(path)
+#    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#    paintings = []
+#    paintings = get_bbox(image)
+#    
+#    for p in paintings:
+#        obtained_text = text_recognition(gray[p[1]:p[3], p[0]:p[2]], config, True)
+#        print(obtained_text)
+#        if paintings.index(p)==0:
+#            save_text('000%s' % (n), obtained_text, "w+")
+#        if paintings.index(p)!=0:
+#            save_text('000%s' % (n), obtained_text, "a")
