0aceb0b464f19b9b733ddd4c11de7d0131d6699d
mariavila <mariava.1213@gmail.com>
2019-10-25 12:28:20 +0200

    standarize hue

--- src/denoising.py
+++ src/denoising.py
@@ -27,6 +27,7 @@ def remove_salt_and_pepper_noise(image):
     #Using median filter
     kernel = 3
     image_filtered = cv2.medianBlur(image,kernel)
+    #TODO: try with openings and closings
     return image_filtered
 
 def remove_hue_variation(image, mean_hue):
@@ -35,8 +36,15 @@ def remove_hue_variation(image, mean_hue):
     Args:
         - image: image in BGR color space
     Returns
-        Image with standarized hue
+        Image in BGR color space with standarized hue
     """
 
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    hue = image[:,:,0]
+    mean = np.mean(hue)
+    shift = mean_hue - mean
+    new_hue = (hue+shift)%180
     image_filtered = image
+    image_filtered[:,:,0] = new_hue
+    image_filtered = cv2.cvtColor(image_filtered, cv2.COLOR_HSV2BGR)
     return image_filtered
