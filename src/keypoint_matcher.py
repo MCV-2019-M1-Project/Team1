0aa0b0b873782b1b1289ad0d176d275516ec9680
Ruben Bagan Benavides <ruben.bagan@gmail.com>
2019-11-02 14:29:09 +0100

    fixes the K for FLANN

--- src/keypoint_matcher.py
+++ src/keypoint_matcher.py
@@ -41,15 +41,15 @@ def BFM(des1, des2, norm_type, max_distance_to_consider_match=1.0):
     return n_matches, mean_cor
 
 
-def FLANN(des1, des2, FLANN_INDEX_KDTREE=1, trees=5, checks=50, K_MATCHES=2, max_distance_to_consider_match=1.0):    
+def FLANN(des1, des2, FLANN_INDEX_KDTREE=0, trees=5, checks=50, K_MATCHES=2, max_distance_to_consider_match=1.0):    
     FLANN_INDEX_KDTREE = 1
     index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
     search_params = dict(checks=50)   # or pass empty dictionary
 
     flann = cv2.FlannBasedMatcher(index_params,search_params)
-    matches = flann.knnMatch(des1,des2,K_MATCHES=2)
+    matches = flann.knnMatch(des1,des2,k=2)
     
-    # Apply ratio test SEE THE TUTORIAL ON THE TOP OF THIS FILE TO SEE WHAT IT DO
+    # Apply ratio test SEE THE TUTORIAL ON THE TOP OF THIS FILE TO SEE WHAT IT DOES
     good_matches = []
     for m,n in matches:
         if m.distance < max_distance_to_consider_match*n.distance:
