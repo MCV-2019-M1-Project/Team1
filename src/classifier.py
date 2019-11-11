86e3bcd7be0df4978aa4661cd80c6342571f074f
mariavila <mariava.1213@gmail.com>
2019-11-11 12:09:46 +0100

    Classifier of ddbb images finished

--- src/classifier.py
+++ src/classifier.py
@@ -219,6 +219,22 @@ def hsv_mean_variance_gradient_mean_angle_descriptors(bbdd_paintings):
     sobel_phase_mean_descriptor = normalize(sobel_phase_mean_descriptor, 0.0, 1.0)
     sobel_phase_variance_descriptor = normalize(sobel_phase_variance_descriptor, 0.0, 1.0)
 
+    #Weight descriptors
+    hue_mean_descriptor = [v * 4.0 for v in hue_mean_descriptor]
+    hue_variance_descriptor = hue_variance_descriptor
+
+    saturation_mean_descriptor = saturation_mean_descriptor
+    saturation_variance_descriptor = saturation_variance_descriptor
+
+    value_mean_descriptor = [v * 2.0 for v in value_mean_descriptor]
+    value_variance_descriptor = value_variance_descriptor
+
+    laplacian_mean_descriptor = [v * 3.0 for v in laplacian_mean_descriptor]
+    laplacian_variance_descriptor = laplacian_variance_descriptor
+
+    sobel_phase_mean_descriptor = [v * 4.0 for v in sobel_phase_mean_descriptor]
+    sobel_phase_variance_descriptor = sobel_phase_variance_descriptor
+
     #Join descriptors and return
     descriptors = []
     for i in range(len(hue_mean_descriptor)):
@@ -226,11 +242,14 @@ def hsv_mean_variance_gradient_mean_angle_descriptors(bbdd_paintings):
         descriptor.append(hue_mean_descriptor[i])
         descriptor.append(hue_variance_descriptor[i])
 
+        descriptor.append(value_mean_descriptor[i])
+        #descriptor.append(value_variance_descriptor[i])
+
         descriptor.append(laplacian_mean_descriptor[i])
-        descriptor.append(laplacian_variance_descriptor[i])
+        #descriptor.append(laplacian_variance_descriptor[i])
 
         descriptor.append(sobel_phase_mean_descriptor[i])
-        descriptor.append(sobel_phase_variance_descriptor[i])
+        #descriptor.append(sobel_phase_variance_descriptor[i])
 
         descriptors.append(descriptor)
     return descriptors
